d3abd96c98e58f2809b5cbfb25849238c7515846
Petr Povazhniy <ess_service@hotmail.com>
2017-05-21 21:31:25 +0300

    its work

---
+++ wwwroot/app/item.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var Item=function(){function Item(id,title,description){this.id=id,this.title=title,this.description=description}return Item}();exports.Item=Item;
+//# sourceMappingURL=item.js.map
