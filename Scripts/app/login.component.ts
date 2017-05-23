38ed78a02a2cd1b8165331e2a9040dd706edd02b
Petr Povazhniy <ess_service@hotmail.com>
2017-05-23 22:54:30 +0300

    routes

---
+++ Scripts/app/login.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from "@angular/core";
+@Component({
+    selector: "login",
+    template: `
+        <h1>{{title}}</h1>
+        <div>
+            <p>This is my login page</p>
+        </div>
+        `
+})
+export class LoginComponent {
+    title = "Login page";
+}
---
+++ Scripts/app/page-not-found.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from "@angular/core";
+@Component({
+    selector: "page-not-found",
+    template: `
+        <h1>{{title}}</h1>
+        <div>
+            <p>This page was not found there...</p>
+        </div>
+        `
+})
+export class PageNotFoundComponent {
+    title = "404 page";
+}
