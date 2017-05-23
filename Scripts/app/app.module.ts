cd48c0ff0a592fde76ca64c0b9ba52d215943a66
Petr Povazhniy <ess_service@hotmail.com>
2017-05-23 22:06:36 +0300

    commit to marge branches

--- Scripts/app/app.module.ts
+++ Scripts/app/app.module.ts
@@ -1,14 +1,16 @@
 ﻿///<reference path="../../typings/index.d.ts"/>
-import {NgModule} from "@angular/core";
-import {BrowserModule} from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
 
 import "rxjs/Rx";
 
-import {AppComponent} from "./app.component";
+import { AppComponent } from "./app.component";
 import { ItemListComponent } from "./item-list.component";
-import {ItemDetailComponent} from "./item-detail.component"
+import { ItemDetailComponent } from "./item-detail.component"
 import { ItemService } from "./item.service";
 
 @NgModule({
@@ -22,7 +24,8 @@ import { ItemService } from "./item.service";
     imports: [
         BrowserModule,
         HttpModule,
-        FormsModule
+        FormsModule,
+        RouterModule
     ],
     // providers
     providers: [
--- wwwroot/systemjs.config.js
+++ wwwroot/systemjs.config.js
@@ -1,35 +1,36 @@
 ﻿(function (global) {
     // map tells the System loader where to look for things
     var map = {
-        'app': 'app', // our application files
-        '@angular': 'js/@angular', // angular2 packages
-        'rxjs': 'js/rxjs' // Rxjs package
+        'app': "app", // our application files
+        '@angular': "js/@angular", // angular2 packages
+        'rxjs': "js/rxjs" // Rxjs package
     };
 
     // packages tells the System loader which filename and/or extensions to look for by default (when none are specified)
     var packages = {
-        'app': { main: 'main.js', defaultExtension: 'js' },
-        'rxjs': { defaultExtension: 'js' }
+        'app': { main: "main.js", defaultExtension: "js" },
+        'rxjs': { defaultExtension: "js" }
     };
 
     // configure @angular packages
     var ngPackageNames = [
-      'common',
-      'compiler',
-      'core',
-      'http',
-      'platform-browser',
-      'platform-browser-dynamic',
-      'upgrade',
-      "forms"
+        "common",
+        "compiler",
+        "core",
+        "http",
+        "platform-browser",
+        "platform-browser-dynamic",
+        "upgrade",
+        "forms",
+        "router"
     ];
 
     function packIndex(pkgName) {
-        packages['@angular/' + pkgName] = { main: 'index.js', defaultExtension: 'js' };
+        packages["@angular/" + pkgName] = { main: "index.js", defaultExtension: "js" };
     }
 
     function packUmd(pkgName) {
-        packages['@angular/' + pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
+        packages["@angular/" + pkgName] = { main: "/bundles/" + pkgName + ".umd.js", defaultExtension: "js" };
     };
 
     var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
