ea5e07d01b875e6cd2cea1b37291a9b57d325ef9
Petr Povazhniy <ess_service@hotmail.com>
2017-05-28 17:53:29 +0300

    Entity Start

---
+++ Model/Comments/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Web.Model.Comments
+{
+    public class Comment
+    {
+        public Comment()
+        {
+        }
+
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public string Text { get; set; }
+        public int Type { get; set; }
+        public int Flags { get; set; }
+        public string UserId { get; set; }
+        public int? ParentId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
---
+++ Model/Items/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Web.Model.Items
+{
+    public class Item
+    {
+        public Item()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Text { get; set; }
+        public string Notes { get; set; }
+        public int Type { get; set; }
+        public int Flags { get; set; }
+        public string UserId { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
